fc138022662d8c53c144431cbfc9c1e464ce7a95
Arifur999 <arr96777@gmail.com>
2026-01-27 23:53:00 +0600

    add admin course approve route and checking

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import cors from "cors";
 import authRouter from './module/auth/auth.route';
 import adminRoutes from './module/admin/admin.route';
 import trainerRoutes from './module/course/trainer.route';
-
+import adminCourseRoutes from './module/adminCourse/admin.course.route'
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/admin", adminRoutes);
 app.use("/api/v1/trainer", trainerRoutes);
-
+app.use("/api/v1/admin", adminCourseRoutes);
 
 
 
