ab8aa94b708da0e63374058b90dbbdbc2397a32a
Arifur999 <arr96777@gmail.com>
2026-01-27 19:59:34 +0600

    register api complete

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
-
+import cors from "cors";
+import authRouter from './module/auth/auth.route';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 
-
+app.use("/api/v1/auth", authRouter);
 
 
 
