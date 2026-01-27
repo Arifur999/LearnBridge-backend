8a247da97d250cea2b75326deed5cca11b5dcef9
Arifur999 <arr96777@gmail.com>
2026-01-27 20:48:25 +0600

    varify token

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from "cors";
 import authRouter from './module/auth/auth.route';
+import { verifyToken } from './middlewares/verifyToken';
 
 const app = express();
 app.use(cors());
@@ -9,7 +10,12 @@ app.use(express.json());
 
 app.use("/api/v1/auth", authRouter);
 
-
+app.get("/api/v1/protected", verifyToken, (req, res) => {
+  res.json({
+    success: true,
+    message: "You are authorized",
+  });
+});
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
